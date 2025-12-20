25f22e81676e8d93efbf446007ed1394dec186b8
Igor <89027688149@ya.ru>
2025-12-20 21:16:28 +0800

    Adding a new settings item to the "Appearance" block - Indentation from the bottom

--- app/src/main/java/com/example/composercalculator/view/components/settings/SettingsBlock/BlockAppearance.kt
+++ app/src/main/java/com/example/composercalculator/view/components/settings/SettingsBlock/BlockAppearance.kt
@@ -1,6 +1,8 @@
 package com.example.composercalculator.view.components.settings.SettingsBlock
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
@@ -38,5 +40,29 @@ fun Appearance(
                 )
             )
         }
+
+        SettingsRow(
+            title = "Отступ снизу",
+            subtitle = "Отступ между элементами интерфейса и нижним краем экрана",
+            modifier = modifier.padding(vertical = 4.dp)
+        ) {
+            Slider(
+//                value = displayFontSize.value,
+                value = 0f,
+//                onValueChangeFinished = { viewModelSettings.onFontSizeChange(displayFontSize.value) },
+                onValueChangeFinished = {  },
+//                onValueChange = { viewModelSettings.onFontSizeChange(it) },
+                onValueChange = {  },
+                valueRange = 40f..120f,
+                steps = 7, // 8 позиций
+                colors = SliderDefaults.colors(
+                    thumbColor = Color.White,
+                    activeTrackColor = Orange,
+                    inactiveTrackColor = Color.Gray
+                ),
+                modifier = Modifier.weight(weight = 1f)
+            )
+        }
+
     }
 }

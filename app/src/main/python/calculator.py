5dbfcf083bc27f8d7bb58261b15cf154e7bdb281
Igor <89027688149@ya.ru>
2025-12-20 21:45:39 +0800

    Updating the python script for calculating the expression

--- app/src/main/python/calculator.py
+++ app/src/main/python/calculator.py
@@ -2,7 +2,13 @@ from simpleeval import simple_eval
 
 
 def evaluate_expression(expression_string: str):
+    def expressionProcessing(expression_string: str) -> str:
+        return (expression_string
+                .replace("^", "**")
+                )
+
     try:
-        return simple_eval(expression_string)
+        processedExpression = expressionProcessing(expression_string=expression_string)
+        return simple_eval(processedExpression)
     except Exception as e:
         return f"Ошибка при оценке выражения: {e}"

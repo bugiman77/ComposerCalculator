633fd2f386d1e5e3487382e59ee49374073c706f
Igor <89027688149@ya.ru>
2025-12-23 19:04:52 +0800

    Adding new colors

--- app/src/main/java/com/example/composercalculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/composercalculator/ui/theme/Color.kt
@@ -5,3 +5,6 @@ import androidx.compose.ui.graphics.Color
 val Orange = Color(color = 0xFFFF9F0A)
 val LightGray = Color(color = 0xFF5C5C5E)
 val DarkGray = Color(color = 0xFF2B2B2D)
+
+val iOSGreen = Color(color = 0xFF34C759)
+val iOSGray = Color(color = 0xFF39393D)

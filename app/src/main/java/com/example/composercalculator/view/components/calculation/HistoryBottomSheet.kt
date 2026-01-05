66fbde7cb614c99fa688db7547f6470815caf7c2
Igor <89027688149@ya.ru>
2026-01-05 17:11:41 +0800

    Adding a swipe to an item in the calculation history list

--- app/src/main/java/com/example/composercalculator/view/components/calculation/HistoryBottomSheet.kt
+++ app/src/main/java/com/example/composercalculator/view/components/calculation/HistoryBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.example.composercalculator.view.components.calculation
 
+import android.R
 import android.content.ClipData
 import android.icu.util.Calendar
 import androidx.compose.foundation.background
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,15 +21,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -138,6 +147,7 @@ fun HistoryBottomSheet(
                         item = item,
                         isNoteEnabled = settingsViewModel.isNoteEnabled.collectAsState().value,
                         calculatorViewModel = calculatorViewModel,
+                        settingsViewModel = settingsViewModel,
                     )
                 }
             }
@@ -146,183 +156,232 @@ fun HistoryBottomSheet(
     }
 }
 
-private fun formatDateForHeader(dateString: String): String {
-    val parser = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    val date = parser.parse(dateString) ?: return dateString
-
-    val today = Calendar.getInstance()
-    val yesterday = Calendar.getInstance().apply { add(Calendar.DAY_OF_YEAR, -1) }
-    val itemCalendar = Calendar.getInstance().apply { time = date }
-
-    return when {
-        today.get(Calendar.YEAR) == itemCalendar.get(Calendar.YEAR) &&
-                today.get(Calendar.DAY_OF_YEAR) == itemCalendar.get(Calendar.DAY_OF_YEAR) -> "Сегодня"
-
-        yesterday.get(Calendar.YEAR) == itemCalendar.get(Calendar.YEAR) &&
-                yesterday.get(Calendar.DAY_OF_YEAR) == itemCalendar.get(Calendar.DAY_OF_YEAR) -> "Вчера"
-
-        else -> SimpleDateFormat("d MMMM yyyy", Locale("ru")).format(date)
-    }
-}
-
 @Composable
 private fun HistoryItemRow(
     item: History,
     isNoteEnabled: Boolean,
     calculatorViewModel: CalculatorViewModel,
+    settingsViewModel: SettingsViewModel,
 ) {
 
     var labelText by remember(key1 = item.id) { mutableStateOf(value = item.note) }
     val scope = rememberCoroutineScope()
     val clipboard = LocalClipboard.current
 
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-//        horizontalAlignment = Alignment.End
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.End
-        ) {
+    val isSwipeEnable = settingsViewModel.isSwipeEnabled.collectAsState().value
 
-            Text(
-                text = item.expression,
-                color = Color.Gray,
-                fontSize = 16.sp
-            )
-            Text(
-                text = "=${item.result}",
-                color = Color.White,
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Medium
-            )
-        }
-
-        Spacer(modifier = Modifier.height(height = 8.dp))
-
-        Text(
-            text = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
-                .withZone(ZoneId.systemDefault()).format(Instant.ofEpochMilli(item.timestamp)),
-            color = Color.Gray,
-            fontSize = 12.sp,
-            modifier = Modifier
-        )
-
-        Spacer(modifier = Modifier.height(height = 8.dp))
-
-        if (isNoteEnabled) {
-
-            OutlinedTextField(
-                value = labelText,
-                onValueChange = { newText ->
-                    labelText = newText
-                    scope.launch {
-                        calculatorViewModel.onInputNote(itemHistory = item, newNote = newText)
-                    }
-                },
-                placeholder = { Text(text = "Добавить заметку...", color = Color.Gray) },
-                modifier = Modifier.fillMaxWidth(),
-                singleLine = true,
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    focusedBorderColor = Orange.copy(alpha = 0.5f),
-                    unfocusedBorderColor = Color.Gray.copy(alpha = 0.3f),
-                    cursorColor = Orange,
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White,
-                    focusedPlaceholderColor = Color.Gray,
-                    unfocusedPlaceholderColor = Color.Gray
-                ),
-                shape = RoundedCornerShape(size = 12.dp)
-            )
-
-            Spacer(
-                modifier = Modifier.height(height = 8.dp)
-            )
-
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-
-            Button(
-                onClick = {
-                    calculatorViewModel.editingExpression(itemHistory = item)
-                },
-                modifier = Modifier.weight(weight = 1f),
-                shape = RoundedCornerShape(size = 12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(color = 0xFFDEAA45)
-                ),
-                contentPadding = PaddingValues(vertical = 8.dp)
-            ) {
-                Text(text = "Изменить", color = Color.White, fontSize = 13.sp)
-            }
-
-            Spacer(
-                modifier = Modifier.width(width = 8.dp)
-            )
-
-            Button(
-                onClick = { calculatorViewModel.deleteHistoryItem(itemHistory = item) },
-                modifier = Modifier.weight(weight = 1f),
-                shape = RoundedCornerShape(size = 12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(color = 0xFFEE4848)
-                ),
-                contentPadding = PaddingValues(vertical = 8.dp)
-            ) {
-                Text(text = "Удалить", color = Color.White, fontSize = 13.sp)
-            }
-
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-
-            Button(
-                onClick = {
+    // Состояние свайпа
+    val dismissState = rememberSwipeToDismissBoxState(
+        confirmValueChange = { value ->
+            when (value) {
+                SwipeToDismissBoxValue.StartToEnd -> {
+                    // Действие при свайпе слева направо (например, копирование результата)
                     scope.launch {
                         val clipEntry =
-                            ClipEntry(ClipData.newPlainText("Expression", item.expression))
+                            ClipEntry(ClipData.newPlainText("Result", item.result))
                         clipboard.setClipEntry(clipEntry)
                     }
-                },
-                modifier = Modifier.weight(weight = 1f), // Занимает 1 долю (половину)
-                shape = RoundedCornerShape(size = 12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(color = 0xFF757575)
-                ),
-                contentPadding = PaddingValues(vertical = 8.dp)
-            ) {
-                Text(text = "Скопировать выражение", color = Color.White, fontSize = 13.sp)
+                    false // Возвращаем карточку на место
+                }
+
+                SwipeToDismissBoxValue.EndToStart -> {
+                    // Действие при свайпе справа налево (например, удаление)
+                    calculatorViewModel.deleteHistoryItem(itemHistory = item)
+                    true
+                }
+
+                else -> false
             }
-
-            Spacer(
-                modifier = Modifier.width(width = 8.dp)
-            )
-
-            Button(
-                onClick = {
-                    scope.launch {
-                        val clipEntry = ClipEntry(ClipData.newPlainText("Result", item.result))
-                        clipboard.setClipEntry(clipEntry)
-                    }
-                },
-                modifier = Modifier.weight(weight = 1f), // Занимает 1 долю (половину)
-                shape = RoundedCornerShape(size = 12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(color = 0xFF616161)
-                ),
-                contentPadding = PaddingValues(vertical = 8.dp)
-            ) {
-                Text(text = "Скопировать результат", color = Color.White, fontSize = 13.sp)
-            }
-
         }
+    )
 
+    SwipeToDismissBox(
+        state = dismissState,
+        enableDismissFromStartToEnd = false, // Слева направо
+        enableDismissFromEndToStart = isSwipeEnable, // Справа налево
+        backgroundContent = {
+            // Фоновый слой (иконки/цвета при свайпе)
+            val direction = dismissState.dismissDirection
+            val color = when (direction) {
+                SwipeToDismissBoxValue.StartToEnd -> Color(color = 0xFF4CAF50) // Зеленый
+                SwipeToDismissBoxValue.EndToStart -> Color(color = 0xFFEE4848) // Красный
+                else -> Color.Transparent
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(shape = RoundedCornerShape(size = 12.dp))
+                    .background(color)
+                    .padding(horizontal = 20.dp),
+                contentAlignment = if (direction == SwipeToDismissBoxValue.StartToEnd)
+                    Alignment.CenterStart else Alignment.CenterEnd
+            ) {
+                val icon = if (direction == SwipeToDismissBoxValue.StartToEnd)
+                    Icons.Default.Delete else Icons.Default.Delete
+                Icon(imageVector = icon, contentDescription = null, tint = Color.White)
+            }
+        }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = Color(color = 0xFF1C1C1E),
+            shape = RoundedCornerShape(size = 12.dp)
+        ) {
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                ) {
+
+                    Text(
+                        text = item.expression,
+                        color = Color.Gray,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "=${item.result}",
+                        color = Color.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(height = 8.dp))
+
+                Text(
+                    text = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+                        .withZone(ZoneId.systemDefault())
+                        .format(Instant.ofEpochMilli(item.timestamp)),
+                    color = Color.Gray,
+                    fontSize = 12.sp,
+                    modifier = Modifier
+                )
+
+                Spacer(modifier = Modifier.height(height = 8.dp))
+
+                if (isNoteEnabled) {
+
+                    OutlinedTextField(
+                        value = labelText,
+                        onValueChange = { newText ->
+                            labelText = newText
+                            scope.launch {
+                                calculatorViewModel.onInputNote(
+                                    itemHistory = item,
+                                    newNote = newText
+                                )
+                            }
+                        },
+                        placeholder = { Text(text = "Добавить заметку...", color = Color.Gray) },
+                        modifier = Modifier.fillMaxWidth(),
+                        singleLine = true,
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedBorderColor = Orange.copy(alpha = 0.5f),
+                            unfocusedBorderColor = Color.Gray.copy(alpha = 0.3f),
+                            cursorColor = Orange,
+                            focusedTextColor = Color.White,
+                            unfocusedTextColor = Color.White,
+                            focusedPlaceholderColor = Color.Gray,
+                            unfocusedPlaceholderColor = Color.Gray
+                        ),
+                        shape = RoundedCornerShape(size = 12.dp)
+                    )
+
+                    Spacer(
+                        modifier = Modifier.height(height = 8.dp)
+                    )
+
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+
+                    Button(
+                        onClick = {
+                            calculatorViewModel.editingExpression(itemHistory = item)
+                        },
+                        modifier = Modifier.weight(weight = 1f),
+                        shape = RoundedCornerShape(size = 20.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(color = 0xFFDEAA45)
+                        ),
+                        contentPadding = PaddingValues(vertical = 8.dp)
+                    ) {
+                        Text(text = "Изменить", color = Color.White, fontSize = 13.sp)
+                    }
+
+                    Spacer(
+                        modifier = Modifier.width(width = 8.dp)
+                    )
+
+                    Button(
+                        onClick = { calculatorViewModel.deleteHistoryItem(itemHistory = item) },
+                        modifier = Modifier.weight(weight = 1f),
+                        shape = RoundedCornerShape(size = 20.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(color = 0xFFEE4848)
+                        ),
+                        contentPadding = PaddingValues(vertical = 8.dp)
+                    ) {
+                        Text(text = "Удалить", color = Color.White, fontSize = 13.sp)
+                    }
+
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                val clipEntry =
+                                    ClipEntry(ClipData.newPlainText("Expression", item.expression))
+                                clipboard.setClipEntry(clipEntry)
+                            }
+                        },
+                        modifier = Modifier.weight(weight = 1f), // Занимает 1 долю (половину)
+                        shape = RoundedCornerShape(size = 20.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(color = 0xFF757575)
+                        ),
+                        contentPadding = PaddingValues(vertical = 8.dp)
+                    ) {
+                        Text(text = "Скопировать выражение", color = Color.White, fontSize = 13.sp)
+                    }
+
+                    Spacer(
+                        modifier = Modifier.width(width = 8.dp)
+                    )
+
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                val clipEntry =
+                                    ClipEntry(ClipData.newPlainText("Result", item.result))
+                                clipboard.setClipEntry(clipEntry)
+                            }
+                        },
+                        modifier = Modifier.weight(weight = 1f), // Занимает 1 долю (половину)
+                        shape = RoundedCornerShape(size = 20.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(color = 0xFF616161)
+                        ),
+                        contentPadding = PaddingValues(vertical = 8.dp)
+                    ) {
+                        Text(text = "Скопировать результат", color = Color.White, fontSize = 13.sp)
+                    }
+
+                }
+
+            }
+        }
     }
 }

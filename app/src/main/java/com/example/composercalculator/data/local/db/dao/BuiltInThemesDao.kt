c83683a47cfbbd4a311e2adbbd54de202f2ab1d2
Igor <89027688149@ya.ru>
2026-01-11 21:41:40 +0800

    Adding actions with built-in themes to the database

--- app/src/main/java/com/example/composercalculator/data/local/db/dao/BuiltInThemesDao.kt
+++ app/src/main/java/com/example/composercalculator/data/local/db/dao/BuiltInThemesDao.kt
@@ -1,8 +1,14 @@
 package com.example.composercalculator.data.local.db.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.composercalculator.data.local.db.entity.BuiltInThemes
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BuiltInThemesDao {
+
+    @Query(value = "SELECT * FROM built_in_themes")
+    fun getThemesAll(): Flow<List<BuiltInThemes>>
 
 }

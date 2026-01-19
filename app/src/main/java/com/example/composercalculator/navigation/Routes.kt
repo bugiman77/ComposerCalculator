50d2f888906f216a188fb2e385a4cbee42586c0a
Igor <89027688149@ya.ru>
2026-01-19 21:45:45 +0800

    Adding new routes

--- app/src/main/java/com/example/composercalculator/navigation/Routes.kt
+++ app/src/main/java/com/example/composercalculator/navigation/Routes.kt
@@ -7,4 +7,8 @@ object Routes {
     const val HISTORY = "history"
     const val PRIVACY_POLICY = "privacy_policy"
     const val CREATE_THEME_USER = "create_theme_user"
+    const val CURRENCY_CONVERTER = "currency_converter"
+    const val DISTANCE_CONVERTER = "distance_converter"
+    const val ENGINEERING_MODE = "engineering_mode"
+    const val SCIENTIFIC_MODE = "scientific_mode"
 }

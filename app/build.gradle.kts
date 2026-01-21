4eae9e1a2501b6d34962a4416eb96c0cb31fe299
Igor <89027688149@ya.ru>
2026-01-21 22:16:12 +0800

    Adding a dependency to download icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,6 +111,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.mediation.test.suite)
     implementation(libs.androidx.compose.adaptive)
     testImplementation(libs.junit)

0c72da0dba86742377d826438cbeb54c8e03ac15
Igor <89027688149@ya.ru>
2026-02-15 21:33:27 +0800

    Add addiction Material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,6 +115,7 @@ dependencies {
     implementation(libs.mediation.test.suite)
     implementation(libs.androidx.compose.adaptive)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

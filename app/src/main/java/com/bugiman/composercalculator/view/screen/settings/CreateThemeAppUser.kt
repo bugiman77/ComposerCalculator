6b4df2b571f40295c5bf82bf1f1f85b36add52f7
Igor <89027688149@ya.ru>
2026-02-28 21:10:53 +0800

    Connecting a component for the top bar

--- app/src/main/java/com/bugiman/composercalculator/view/screen/settings/CreateThemeAppUser.kt
+++ app/src/main/java/com/bugiman/composercalculator/view/screen/settings/CreateThemeAppUser.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bugiman.composercalculator.view.components.general.settings.CustomTopBar
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable

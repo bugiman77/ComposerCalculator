dcfbef1bf7bf0f7674ff56a3202dfb3ab63b0cec
Igor <89027688149@ya.ru>
2026-03-08 23:33:47 +0800

    Add proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
     id("com.chaquo.python") version "15.0.1" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
 }

ed54e9d3c3cb13b9327a52a47f6e075d5739d13a
Igor <89027688149@ya.ru>
2026-03-09 20:44:25 +0800

    Extension methods for converting from proto to domain and vice versa

--- data/src/main/java/com/bugiman/data/mapper/SettingMapper.kt
+++ data/src/main/java/com/bugiman/data/mapper/SettingMapper.kt
@@ -30,29 +30,21 @@ fun SettingEntity.toDomain(): SettingModel {
     )
 }
 
-fun SettingModel.toEntity(): SettingEntity {
-    return SettingEntity(
-        id = id,
-        isDarkTheme = isDarkTheme,
-        isSystemTheme = isSystemTheme,
-        showHistoryButton = showHistoryButton,
-        systemFontSize = systemFontSize,
-        displayFontSize = displayFontSize,
-        decimalFormat = decimalFormat,
-        isSaveHistoryData = isSaveHistoryData,
-        isSaveSettingsData = isSaveSettingsData,
-        isSwipeEnabled = isSwipeEnabled,
-        isNoteEnabled = isNoteEnabled,
-        showIconButton = showIconButton,
-        playSound = playSound,
-        playVibration = playVibration,
-        bottomSpacer = bottomSpacer,
-        isAnimationAll = isAnimationAll,
-        keepScreenOn = keepScreenOn,
-        showPlaceholderInput = showPlaceholderInput,
-        historyHeaderLayout = historyHeaderLayout,
-        isTitleNote = isTitleNote,
-        isClearHistoryOnClose = isClearHistoryOnClose,
-        isShowHistoryLastCalculation = isShowHistoryLastCalculation,
-    )
+fun SettingModel.toProto(currentProto: SettingsProto): SettingsProto {
+    return currentProto.toBuilder()
+        .setIsDarkTheme(this.isDarkTheme)
+        .setIsSystemTheme(this.isSystemTheme)
+        .setIsShowHistoryButton(this.isShowHistoryButton)
+        .setIsSwipeEnabled(this.isSwipeEnabled)
+        .setIsNoteEnabled(this.isNoteEnabled)
+        .setIsShowIconButton(this.isShowIconButton)
+        .setIsPlaySound(this.isPlaySound)
+        .setIsPlayVibration(this.isPlayVibration)
+        .setIsKeepScreenOn(this.isKeepScreenOn)
+        .setIsShowPlaceholderInput(this.isShowPlaceholderInput)
+        .setIsTitleNote(this.isTitleNote)
+        .setIsClearHistoryOnClose(this.isClearHistoryOnClose)
+        .setIsShowHistoryLastCalculation(this.isShowHistoryLastCalculation)
+        .setHeaderLayoutValue(this.historyHeaderLayout)
+        .build()
 }

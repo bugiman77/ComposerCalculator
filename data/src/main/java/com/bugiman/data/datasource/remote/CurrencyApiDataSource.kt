d245680f0b395dffa7e3903c74c31f29691a7168
Igor <89027688149@ya.ru>
2026-03-15 20:56:30 +0800

    CurrencyApiDataSource.kt

--- data/src/main/java/com/bugiman/data/datasource/remote/CurrencyApiDataSource.kt
+++ data/src/main/java/com/bugiman/data/datasource/remote/CurrencyApiDataSource.kt
@@ -1,4 +1,24 @@
 package com.bugiman.data.datasource.remote
 
-class CurrencyApiDataSource {
+import com.bugiman.data.BuildConfig
+import com.bugiman.data.network.CurrencyApiService
+
+class CurrencyApiDataSource(
+    private val apiService: CurrencyApiService
+) {
+    private val apiKey = BuildConfig.CURRENCY_API_KEY
+
+    suspend fun fetchPairRate(from: String, to: String): Double {
+        return try {
+            val response = apiService.getPairRate(
+                apiKey = apiKey,
+                fromCurrency = from,
+                toCurrency = to
+            )
+
+            response.conversionRate
+        } catch (e: Exception) {
+            0.0
+        }
+    }
 }

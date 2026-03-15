d6b8d8e543465d37f214707bf7ba777937bc1f3a
Igor <89027688149@ya.ru>
2026-03-15 20:55:50 +0800

    HistoryItemSaveUseCase.kt

---
+++ domain/src/main/java/com/bugiman/domain/usecase/history/HistoryItemSaveUseCase.kt
@@ -0,0 +1,14 @@
+package com.bugiman.domain.usecase.history
+
+import com.bugiman.domain.models.history.HistoryModel
+import com.bugiman.domain.repository.history.HistoryRepository
+
+class HistoryItemSaveUseCase(
+    private val repository: HistoryRepository
+) {
+
+    suspend operator fun invoke(historyModel: HistoryModel) {
+        repository.saveItem(historyModel = historyModel)
+    }
+
+}

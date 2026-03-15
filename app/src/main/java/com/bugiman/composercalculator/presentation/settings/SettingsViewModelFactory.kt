24b7bd9e6a5de93e602e978bebecd501f19e0dd6
Igor <89027688149@ya.ru>
2026-03-15 21:01:10 +0800

    Renaming variables

--- app/src/main/java/com/bugiman/composercalculator/presentation/settings/SettingsViewModelFactory.kt
+++ app/src/main/java/com/bugiman/composercalculator/presentation/settings/SettingsViewModelFactory.kt
@@ -6,8 +6,8 @@ import com.bugiman.domain.usecase.settings.SettingsAllGetUseCase
 import com.bugiman.domain.usecase.settings.SettingsItemUpdateUseCase
 
 class SettingsViewModelFactory(
-    private val getSettingsUseCase: SettingsAllGetUseCase,
-    private val updateSettingsUseCase: SettingsItemUpdateUseCase
+    private val settingsAllGetUseCase: SettingsAllGetUseCase,
+    private val settingsItemUpdateUseCase: SettingsItemUpdateUseCase
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
@@ -15,8 +15,8 @@ class SettingsViewModelFactory(
         // Проверяем, что создается именно наша SettingsViewModel
         if (modelClass.isAssignableFrom(SettingsViewModel::class.java)) {
             return SettingsViewModel(
-                getSettingsUseCase = getSettingsUseCase,
-                updateSettingsUseCase = updateSettingsUseCase
+                getSettingsUseCase = settingsAllGetUseCase,
+                updateSettingsUseCase = settingsItemUpdateUseCase
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")

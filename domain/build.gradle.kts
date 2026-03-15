de08a358aaa6537c2c92f7f4436b5e5872bd5a59
Igor <89027688149@ya.ru>
2026-03-15 17:45:58 +0800

    Update build.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,18 +1,19 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_17
     }
 }
 
 dependencies {
-//    implementation(libs.kotlinx.coroutines.core)
     api(libs.kotlinx.coroutines.core)
 }

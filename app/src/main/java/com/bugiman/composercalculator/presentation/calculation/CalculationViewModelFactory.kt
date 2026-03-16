32b61e84d102e148b2a78721baeea95643b1707f
Igor <89027688149@ya.ru>
2026-03-16 21:41:46 +0800

    CalculationViewModelFactory.kt

--- app/src/main/java/com/bugiman/composercalculator/presentation/calculation/CalculationViewModelFactory.kt
+++ app/src/main/java/com/bugiman/composercalculator/presentation/calculation/CalculationViewModelFactory.kt
@@ -2,18 +2,14 @@ package com.bugiman.composercalculator.presentation.calculation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.bugiman.composercalculator.core.managers.SoundManager
-import com.bugiman.composercalculator.core.managers.VibrationManager
-import com.bugiman.composercalculator.presentation.settings.SettingsViewModel
 import com.bugiman.domain.usecase.calculation.CalculateExpressionUseCase
+import com.bugiman.domain.usecase.feedback.TriggerFeedbackUseCase
 import com.bugiman.domain.usecase.history.HistoryItemSaveUseCase
 
 class CalculationViewModelFactory(
     private val calculateExpressionUseCase: CalculateExpressionUseCase,
     private val historyItemSaveUseCase: HistoryItemSaveUseCase,
-    private val settingsViewModel: SettingsViewModel,
-    private val soundManager: SoundManager,
-    private val vibrationManager: VibrationManager
+    private val triggerFeedbackUseCase: TriggerFeedbackUseCase
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
@@ -23,9 +19,7 @@ class CalculationViewModelFactory(
             return CalculatorViewModel(
                 calculateExpressionUseCase = calculateExpressionUseCase,
                 historyItemSaveUseCase = historyItemSaveUseCase,
-                settingsViewModel = settingsViewModel,
-                soundManager = soundManager,
-                vibrationManager = vibrationManager
+                triggerFeedbackUseCase = triggerFeedbackUseCase
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")

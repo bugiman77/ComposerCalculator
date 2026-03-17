38832a99c97dab406c91fb0fe13d293b351eef15
Igor <89027688149@ya.ru>
2026-03-17 23:00:39 +0800

    HistoryViewModelFactory.kt

--- app/src/main/java/com/bugiman/composercalculator/presentation/history/HistoryViewModelFactory.kt
+++ app/src/main/java/com/bugiman/composercalculator/presentation/history/HistoryViewModelFactory.kt
@@ -1,4 +1,44 @@
 package com.bugiman.composercalculator.presentation.history
 
-class HistoryViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bugiman.domain.usecase.history.HistoryAllDeleteUseCase
+import com.bugiman.domain.usecase.history.HistoryAllGetUseCase
+import com.bugiman.domain.usecase.history.HistoryCountUseCase
+import com.bugiman.domain.usecase.history.HistoryItemCopyExpressionUseCase
+import com.bugiman.domain.usecase.history.HistoryItemCopyResultUseCase
+import com.bugiman.domain.usecase.history.HistoryItemDeleteUseCase
+import com.bugiman.domain.usecase.history.HistoryItemEditUseCase
+import com.bugiman.domain.usecase.history.HistoryItemSaveUseCase
+import com.bugiman.domain.usecase.history.HistoryItemUpdateNoteUseCase
+
+class HistoryViewModelFactory(
+    private val historyAllDeleteUseCase: HistoryAllDeleteUseCase,
+    private val historyAllGetUseCase: HistoryAllGetUseCase,
+    private val historyCountUseCase: HistoryCountUseCase,
+    private val historyItemCopyExpressionUseCase: HistoryItemCopyExpressionUseCase,
+    private val historyItemCopyResultUseCase: HistoryItemCopyResultUseCase,
+    private val historyItemDeleteUseCase: HistoryItemDeleteUseCase,
+    private val historyItemEditUseCase: HistoryItemEditUseCase,
+    private val historyItemSaveUseCase: HistoryItemSaveUseCase,
+    private val historyItemUpdateNoteUseCase: HistoryItemUpdateNoteUseCase
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HistoryViewModel::class.java)) {
+            return HistoryViewModel(
+                historyAllDeleteUseCase = historyAllDeleteUseCase,
+                historyAllGetUseCase = historyAllGetUseCase,
+                historyCountUseCase = historyCountUseCase,
+                historyItemCopyExpressionUseCase = historyItemCopyExpressionUseCase,
+                historyItemCopyResultUseCase = historyItemCopyResultUseCase,
+                historyItemDeleteUseCase = historyItemDeleteUseCase,
+                historyItemEditUseCase = historyItemEditUseCase,
+                historyItemSaveUseCase = historyItemSaveUseCase,
+                historyItemUpdateNoteUseCase = historyItemUpdateNoteUseCase,
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
 }

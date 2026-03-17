6f66d0cd17486772581ba5c959b4748682a61639
Igor <89027688149@ya.ru>
2026-03-17 23:01:57 +0800

    HistoryDao.kt

--- data/src/main/java/com/bugiman/data/repository/converter/ConverterRepositoryImpl.kt
+++ data/src/main/java/com/bugiman/data/repository/converter/ConverterRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.bugiman.data.repository.converter
 
-class ConverterRepositoryImpl {
+import com.bugiman.domain.models.converter.ConverterType
+import com.bugiman.domain.repository.converter.ConverterRepository
+
+class ConverterRepositoryImpl(
+
+) : ConverterRepository {
+    override suspend fun convert(
+        type: ConverterType,
+        value: Double,
+        from: String,
+        to: String
+    ): Result<Double> {
+        TODO("Not yet implemented")
+    }
 }

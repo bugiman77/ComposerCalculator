d793e746e5018db0ed20e11e0880bd832983ec86
Igor <89027688149@ya.ru>
2026-03-20 23:47:35 +0800

    ConverterViewModelFactory.kt

--- app/src/main/java/com/bugiman/composercalculator/presentation/convert/ConverterViewModelFactory.kt
+++ app/src/main/java/com/bugiman/composercalculator/presentation/convert/ConverterViewModelFactory.kt
@@ -3,11 +3,11 @@ package com.bugiman.composercalculator.presentation.convert
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bugiman.domain.usecase.convert.ConvertValueUseCase
-import com.bugiman.domain.usecase.feedback.TriggerFeedbackUseCase
+import com.bugiman.domain.usecase.feedback.FeedbackTriggerUseCase
 
 class ConverterViewModelFactory(
     private val convertValueUseCase: ConvertValueUseCase,
-    private val triggerFeedbackUseCase: TriggerFeedbackUseCase
+    private val feedbackTriggerUseCase: FeedbackTriggerUseCase
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
@@ -16,7 +16,7 @@ class ConverterViewModelFactory(
         if (modelClass.isAssignableFrom(ConverterViewModel::class.java)) {
             return ConverterViewModel(
                 convertValueUseCase = convertValueUseCase,
-                triggerFeedbackUseCase = triggerFeedbackUseCase
+                feedbackTriggerUseCase = feedbackTriggerUseCase
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")

11fb3bc585dcabf2a63b4c4381c53f498c42b595
Igor <89027688149@ya.ru>
2026-04-29 23:29:33 +0800

    fix

--- calculation_app/src/main/java/com/bugiman/composercalculator/presentation/convert/ConvertViewModelFactory.kt
+++ calculation_app/src/main/java/com/bugiman/composercalculator/presentation/convert/ConvertViewModelFactory.kt
@@ -2,20 +2,23 @@ package com.bugiman.composercalculator.presentation.convert
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bugiman.domain.usecase.convert.ConvertGetFormattedConversionUseCase
+import com.bugiman.domain.usecase.convert.ConvertSwapCurrenciesUseCase
 import com.bugiman.domain.usecase.convert.ConvertValueUseCase
 import com.bugiman.domain.usecase.feedback.FeedbackTriggerUseCase
 
 class ConvertViewModelFactory(
-    private val convertValueUseCase: ConvertValueUseCase,
+    private val convertGetFormattedConversionUseCase: ConvertGetFormattedConversionUseCase,
+    private val convertSwapCurrenciesUseCase: ConvertSwapCurrenciesUseCase,
     private val feedbackTriggerUseCase: FeedbackTriggerUseCase
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        // Проверяем, что создается именно ConverterViewModel
         if (modelClass.isAssignableFrom(ConvertViewModel::class.java)) {
             return ConvertViewModel(
-                convertValueUseCase = convertValueUseCase,
+                convertGetFormattedConversionUseCase = convertGetFormattedConversionUseCase,
+                convertSwapCurrenciesUseCase = convertSwapCurrenciesUseCase,
                 feedbackTriggerUseCase = feedbackTriggerUseCase
             ) as T
         }

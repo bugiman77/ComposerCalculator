cb3fef5fb09667f11a7544e1c7e804e49ef17069
Igor <89027688149@ya.ru>
2026-04-29 23:29:21 +0800

    fix

--- calculation_app/src/main/java/com/bugiman/composercalculator/presentation/calculation/CalculationViewModelFactory.kt
+++ calculation_app/src/main/java/com/bugiman/composercalculator/presentation/calculation/CalculationViewModelFactory.kt
@@ -3,6 +3,12 @@ package com.bugiman.composercalculator.presentation.calculation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bugiman.domain.usecase.calculation.CalculateExpressionUseCase
+import com.bugiman.domain.usecase.calculation.CalculationBuildBracketUseCase
+import com.bugiman.domain.usecase.calculation.CalculationBuildDecimalUseCase
+import com.bugiman.domain.usecase.calculation.CalculationBuildDigitUseCase
+import com.bugiman.domain.usecase.calculation.CalculationBuildOperatorUseCase
+import com.bugiman.domain.usecase.calculation.CalculationBuildZeroUseCase
+import com.bugiman.domain.usecase.calculation.CalculationRemoveLastCharUseCase
 import com.bugiman.domain.usecase.feedback.FeedbackTriggerUseCase
 import com.bugiman.domain.usecase.history.HistoryAllDeleteUseCase
 import com.bugiman.domain.usecase.history.HistoryAllGetUseCase
@@ -13,19 +19,30 @@ class CalculationViewModelFactory(
     private val historyItemSaveUseCase: HistoryItemSaveUseCase,
     private val feedbackTriggerUseCase: FeedbackTriggerUseCase,
     private val historyAllGetUseCase: HistoryAllGetUseCase,
-    private val historyAllDeleteUseCase: HistoryAllDeleteUseCase
+    private val historyAllDeleteUseCase: HistoryAllDeleteUseCase,
+    private val buildDigitUseCase: CalculationBuildDigitUseCase,
+    private val buildOperatorUseCase: CalculationBuildOperatorUseCase,
+    private val buildBracketUseCase: CalculationBuildBracketUseCase,
+    private val buildDecimalUseCase: CalculationBuildDecimalUseCase,
+    private val buildZeroUseCase: CalculationBuildZeroUseCase,
+    private val removeLastCharUseCase: CalculationRemoveLastCharUseCase
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        // Проверяем, что создается именно наша SettingsViewModel
         if (modelClass.isAssignableFrom(CalculatorViewModel::class.java)) {
             return CalculatorViewModel(
                 calculateExpressionUseCase = calculateExpressionUseCase,
                 historyItemSaveUseCase = historyItemSaveUseCase,
                 feedbackTriggerUseCase = feedbackTriggerUseCase,
                 historyAllGetUseCase = historyAllGetUseCase,
-                historyAllDeleteUseCase = historyAllDeleteUseCase
+                historyAllDeleteUseCase = historyAllDeleteUseCase,
+                buildDigitUseCase = buildDigitUseCase,
+                buildOperatorUseCase = buildOperatorUseCase,
+                buildBracketUseCase = buildBracketUseCase,
+                buildDecimalUseCase = buildDecimalUseCase,
+                buildZeroUseCase = buildZeroUseCase,
+                removeLastCharUseCase = removeLastCharUseCase
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")

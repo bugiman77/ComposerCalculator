0a9ce219c6c567f91545b1f77c1213c7c87ac911
Igor <89027688149@ya.ru>
2026-04-29 23:29:45 +0800

    fix

--- calculation_app/src/main/java/com/bugiman/composercalculator/navigation/AppNavigation.kt
+++ calculation_app/src/main/java/com/bugiman/composercalculator/navigation/AppNavigation.kt
@@ -6,24 +6,23 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.navigator.Navigator
 import com.bugiman.composercalculator.presentation.calculation.CalculatorViewModel
+import com.bugiman.composercalculator.presentation.convert.ConvertViewModel
 import com.bugiman.composercalculator.presentation.settings.SettingsViewModel
 
 @Composable
 fun AppNavigation(
-    settingsViewModel: SettingsViewModel = viewModel(),
-    calculatorViewModel: CalculatorViewModel = viewModel(),
+    settingsViewModel: SettingsViewModel,
+    calculatorViewModel: CalculatorViewModel,
+    converterViewModel: ConvertViewModel,
 ) {
-    // 1. Получаем всё состояние настроек одним объектом
     val settings by settingsViewModel.uiState.collectAsStateWithLifecycle()
-
-    // 2. Достаем нужное поле напрямую из объекта settings
     val isShowHistory = settings.isShowHistoryButton
 
-    // Запускаем Navigator, но внутри используем наш кастомный TelegramSwipeContainer
     Navigator(
         screen = CalculatorScreen(
             settingsViewModel = settingsViewModel,
             calculatorViewModel = calculatorViewModel,
+            converterViewModel = converterViewModel,
             showHistoryButton = isShowHistory
         )
     ) { navigator ->
